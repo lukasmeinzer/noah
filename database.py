9d7a1e681852370c74fbff8c5b4251135dc79b73
lukas <lukas.meinzer.98@web.de>
2024-11-27 08:27:53 +0000

    Neue Struktur

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
-import os
-
-# Database setup with SQLAlchemy
-DATABASE_URL = "sqlite:///offers.db"  # Change this to your database URL
-
-engine = create_engine(DATABASE_URL, echo=True)
-Session = sessionmaker(bind=engine)
